bdf00ebc2ef20066bb7bfa0e138137774a476514
Paul Wissemberg <147991111+pw2sbg@users.noreply.github.com>
2024-06-30 00:20:19 +0200

    Add files via upload

---
+++ config/constants.py
@@ -0,0 +1,16 @@
+tx_csv = 'tx_etf.csv'
+px_csv = 'px_etf.csv'
+
+
+
+tx_headers = ['date', 'ticker', 'qty', 'order']
+
+
+
+start_year_month = 200501
+end_year_month = 202312
+
+
+
+start_year = 2005
+end_year = 2023
